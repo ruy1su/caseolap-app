2ce0187fa6bf8dfa039cacf0866d20356a65777d
Eric <zixia@umail.ucsb.edu>
2019-01-28 02:13:52 -0800

    new ui

--- app.js
+++ app.js
@@ -4,22 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-const assert = require('assert');
 
-// MongoDB Set Up
-var db;
-const MongoClient = require('mongodb').MongoClient;
-const dbName = 'caseolap';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+// app.set('view engine', 'ejs');
 
 
 app.use(logger('dev'));
@@ -28,35 +23,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 
-MongoClient.connect("mongodb://localhost:27017", function (err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
 
-  db = client.db(dbName);
+//   db = client.db(dbName);
 
-	app.listen(process.env.PORT || 8080, () => {
-	    console.log('listening on 3000')
-	})
+	// app.listen(process.env.PORT || 8080, () => {
+	//     console.log('listening on 3000')
+	// })
   // Initialize the app.
   // 	var server = app.listen(process.env.PORT || 3000, function () {
   //   var port = server.address().port;
   //   console.log("App now running on port", port);
   // });
   // client.close();
+// });
+var server = app.listen(process.env.PORT || 8080, function () {
+var port = server.address().port;
+    console.log("App now running on port", port);
 });
 
 
-app.get('/', (req, res) => {
-  db.collection('article').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    console.log(result[0])
-    res.render('index.ejs', {articles: result.slice(1, 100)})
-  })
-})
+// app.get('/', (req, res) => {
+//   db.collection('article').find().toArray((err, result) => {
+//     if (err) return console.log(err)
+//     console.log(result[0])
+//     res.render('index', {articles: result.slice(1, 100)})
+//   })
+// })
 
 
 // catch 404 and forward to error handler
@@ -74,5 +73,5 @@ app.use(function(req, res, next) {
 //   res.status(err.status || 500);
 //   res.render('error');
 // });
-
+// client.close();
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const assert = require('assert');
+// MongoDB Set Up
+var db;
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'caseolap';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+	  assert.equal(null, err);
+	  console.log("Connected successfully to server");
+
+	  db = client.db(dbName);
+	  db.collection('article').find().toArray((err, result) => {
+	    if (err) return console.log(err)
+	    console.log(result[0])
+	    res.render('index', {articles: result.slice(1, 100)})
+	  })
+	});
 });
 
 module.exports = router;
